07bcf42bde0bc6ac02ce5b04a2c411debd881e4b
Maverickto11 <maverick.cordovabus@ug.edu.ec>
2024-04-25 20:42:13 -0500

    link para los proyectos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,13 @@ import { HomeComponent } from "./home/home.component";
 import { SkillComponent } from "./skill/skill.component";
 import { ProjectComponent } from "./project/project.component";
 import { CertificadoComponent } from "./certificado/certificado.component";
+import { FooterComponent } from "./footer/footer.component";
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavegadorComponent, HomeComponent, SkillComponent, ProjectComponent, CertificadoComponent]
+    imports: [RouterOutlet, NavegadorComponent, HomeComponent, SkillComponent, ProjectComponent, CertificadoComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'PortafolioWeb';
